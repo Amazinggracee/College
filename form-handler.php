9e33c9941ab85fd660dc4a1963b5384145e30e09
AMARACHI DIMKPA <107859883+Amazinggracee@users.noreply.github.com>
2022-10-10 13:21:40 +0100

    First Commit

---
+++ form-handler.php
@@ -0,0 +1,30 @@
+<?php
+$name = $_POST['name'];
+$visitor_email = $_POST['email'];
+$subject = $_POST['subject'];
+$message = $_POST['message'];
+
+$email_from = 'info@yourwebsite.com';
+
+$email_subject = 'New Form Submission';
+
+$email_body = "User Name: $name.\n".
+                "User Email: $visitor_email.\n".
+                "Subject: $subject.\n".
+                "Message: $message.\n".;
+
+
+$to = 'abc@gmail.com';
+
+$headers = "From: $email_from\r\n";
+
+$headers = "Reply-To: $visitor_email\r\n";
+
+mail($to, $email_subject, $email_body, $headers);
+
+header("Location: contact.html");
+
+
+
+
+?>
